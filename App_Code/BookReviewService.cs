dc611e85204d86f6b73769bbb57b40e109dce427
Steve Conger <steve@spconger.com>
2017-05-04 12:24:21 -0700

    First Commit

---
+++ App_Code/BookReviewService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookReviewService" in code, svc and config file together.
+public class BookReviewService : IBookReviewService
+{
+    BookReviewDbEntities1 db = new BookReviewDbEntities1();
+    public List<BookInfo> GetBooks()
+    {
+        var bks = from b in db.Books
+                  select b;
+                  
+        List<BookInfo> books = new List<BookInfo>();
+        foreach(Book bk in bks)
+        {
+            BookInfo book1 = new BookInfo();
+            book1.BookTitle = bk.BookTitle;
+            book1.BookEntryDate = bk.BookEntryDate.ToShortDateString();
+            book1.BookISBN = bk.BookISBN;
+
+            book1.BookAuthor = new List<Author> ();
+            foreach(Author a in bk.Authors)
+            {
+                Author au = new Author();
+                au.AuthorName = a.AuthorName;
+                book1.BookAuthor.Add(au);
+            }
+            books.Add(book1);
+        }
+        return books;
+    }
+
+    public BookInfo GetBooksByAuthor(string authorName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Login(string user, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RegisterReviewer(Reviewer r)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ App_Code/IBookReviewService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookReviewService" in both code and config file together.
+[ServiceContract]
+public interface IBookReviewService
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterReviewer(Reviewer r);
+
+    [OperationContract]
+    List<BookInfo> GetBooks();
+
+    [OperationContract]
+    BookInfo GetBooksByAuthor(string authorName);
+   
+}   
+
+[DataContract]
+public class BookInfo
+{
+    
+    [DataMember]
+    public string BookTitle { get; set; }
+
+    [DataMember]
+    public string BookEntryDate { get; set; }
+
+    [DataMember]
+    public string BookISBN { get; set; }
+
+    [DataMember]
+    public List<Author> BookAuthor { get; set; }
+
+}
