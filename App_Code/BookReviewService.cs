11d6ad0bca9a10b909aeaeba378dcdef35937075
Steve Conger <steve@spconger.com>
2017-05-09 11:23:58 -0700

    Commit two

--- App_Code/BookReviewService.cs
+++ App_Code/BookReviewService.cs
@@ -8,13 +8,18 @@ using System.Text;
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookReviewService" in code, svc and config file together.
 public class BookReviewService : IBookReviewService
 {
+    //make a connection to the data entities
     BookReviewDbEntities1 db = new BookReviewDbEntities1();
     public List<BookInfo> GetBooks()
     {
+        //this method returns all the books
         var bks = from b in db.Books
                   select b;
                   
         List<BookInfo> books = new List<BookInfo>();
+        //we loop through the results
+        //and write each field to the BookInfo
+        //class
         foreach(Book bk in bks)
         {
             BookInfo book1 = new BookInfo();
@@ -23,29 +28,66 @@ public class BookReviewService : IBookReviewService
             book1.BookISBN = bk.BookISBN;
 
             book1.BookAuthor = new List<Author> ();
+            //do the same for authors
             foreach(Author a in bk.Authors)
             {
                 Author au = new Author();
                 au.AuthorName = a.AuthorName;
                 book1.BookAuthor.Add(au);
             }
+            //add it to the list
             books.Add(book1);
+        }
+        //return the list
+        return books;
+    }
+
+    public List<Book> GetBooksByAuthor(string authorName)
+    {
+        //this has the same structure as the previous
+        //method except we limit it to a particular author
+        var bks = from b in db.Books
+                  from a in b.Authors
+                  where a.AuthorName.Equals(authorName)
+                  select b;
+        List<Book> books = new List<Book> ();
+
+        foreach(var bk in bks)
+        {
+            Book bo = new Book();
+            bo.BookTitle = bk.BookTitle;
+            bo.BookEntryDate = bk.BookEntryDate;
+            bo.BookISBN = bk.BookISBN;
+            
+            books.Add(bo);
         }
         return books;
     }
 
-    public BookInfo GetBooksByAuthor(string authorName)
-    {
-        throw new NotImplementedException();
-    }
-
     public int Login(string user, string password)
     {
-        throw new NotImplementedException();
+        //just like the login we did in the last assignment
+        int key = 0;
+        int result = db.usp_ReviewerLogin(user, password);
+        if (result != -1)
+        {
+            var userKey = (from k in db.Reviewers
+                           where k.ReviewerUserName.Equals(user)
+                           select k.ReviewerKey).FirstOrDefault();
+            key = (int)userKey;
+        }
+
+        return key;
     }
 
     public bool RegisterReviewer(Reviewer r)
     {
-        throw new NotImplementedException();
+        //register using the stored procedure
+        bool result = true;
+        int rev = db.usp_NewReviewer(r.ReviewerUserName,
+            r.ReviewerFirstName, r.ReviewerLastName,
+            r.ReviewerEmail, r.ReviewPlainPassword);
+
+        return result;
     }
 }
--- App_Code/IBookReviewService.cs
+++ App_Code/IBookReviewService.cs
@@ -5,10 +5,19 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookReviewService" in both code and config file together.
+/// <summary>
+/// An interface is a list of method signatures
+/// a class that implements the interface
+/// must have these methods.
+/// the service sends these method signatures
+/// to the client
+/// </summary>
 [ServiceContract]
 public interface IBookReviewService
 {
+    //a method must be marked
+    //as an operation contract
+    //to be used in the Web client later
     [OperationContract]
     int Login(string user, string password);
 
@@ -19,14 +28,22 @@ public interface IBookReviewService
     List<BookInfo> GetBooks();
 
     [OperationContract]
-    BookInfo GetBooksByAuthor(string authorName);
+    List<Book> GetBooksByAuthor(string authorName);
    
 }   
 
+/// <summary>
+/// a Data contract is a class you design
+/// you can use it like one of the classes
+/// created by the Data Entities wizard
+/// </summary>
 [DataContract]
 public class BookInfo
 {
     
+    //a field must be marked
+    //as a DataMember to be seen
+    //by the client later
     [DataMember]
     public string BookTitle { get; set; }
 
